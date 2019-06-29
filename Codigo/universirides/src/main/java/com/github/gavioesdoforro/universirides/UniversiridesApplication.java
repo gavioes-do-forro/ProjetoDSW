c0aff5575f34a9b565d04f811411af7e419c9f46
Marcos Rafael Lapa de Sousa <marcosrafaellsousa@gmail.com>
2019-06-29 15:39:30 -0300

    Commit inicial do projeto universirides

---
+++ Codigo/universirides/src/main/java/com/github/gavioesdoforro/universirides/UniversiridesApplication.java
@@ -0,0 +1,13 @@
+package com.github.gavioesdoforro.universirides;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversiridesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversiridesApplication.class, args);
+    }
+
+}
