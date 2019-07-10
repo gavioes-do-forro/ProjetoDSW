08dd4219d43ab0167e960bd9b008e36172db61a9
Marcos Rafael Lapa de Sousa <marcosrafaellsousa@gmail.com>
2019-07-10 18:02:50 -0300

    Renomeando classe controller

--- Codigo/universirides/src/main/java/com/github/gavioesdoforro/universirides/controller/CaronaController.java
+++ Codigo/universirides/src/main/java/com/github/gavioesdoforro/universirides/controller/CaronaController.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.ModelAndView;
 import java.util.List;
 
 @Controller
-public class ControllerExampleJSP {
+public class CaronaController {
 
     @Autowired
     private IRepositorioCarona repositorioCarona;
